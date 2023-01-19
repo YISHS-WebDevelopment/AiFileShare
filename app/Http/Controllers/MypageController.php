72c14413a419d5b0762f83288f43a9cf818fac60
DESKTOP-K5MSV98\ghkah <godqhrgksguswnsdl@naver.com>
2023-01-20 00:03:29 +0900

    my_page_remaster_3

--- app/Http/Controllers/MypageController.php
+++ app/Http/Controllers/MypageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Circle;
 use App\User;
 use Illuminate\Http\Request;
 
@@ -10,9 +11,23 @@ class MypageController extends Controller
     //
     public function index(User $user)
     {
-        $post = $user->posts;
-        $post_array = [];
-        return view('mypage/index', compact(['user']));
+        $posts = $user->posts;
+        $circles = Circle::all();
+        foreach ($circles as $circle) {
+            $post_arr[$circle->detail] = [];
+        }
+        foreach ($posts as $post) {
+            if ($post->circle_check === 'null') {
+                array_push($post_arr['all'], $post);
+            } else {
+                foreach ($post_arr as $key => $circle) {
+                    if ($post->circle_check == $key) {
+                        array_push($post_arr[$key],$post);
+                    }
+                }
+            }
+        }
+        return view('mypage/index', compact(['user','post_arr']));
     }
 
     public function update(Request $request, User $user)
